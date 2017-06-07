d149f4660ce9962a66b6754114ed7b6abe10bbd1
Pany Koutsios <pkoutsios@hotmail.com>
2017-06-07 18:48:11 +0100

    Added app setting for test

---
+++ FilmLibrary.Test/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmLibrary.Test
+{
+    public class AppSettings
+    {
+        public string Themoviedb_apikey { get; set; }
+        public string Proxy_url { get; set; }
+        public string Proxy_username { get; set; }
+        public string Proxy_password { get; set; }
+    }
+}
--- FilmLibrary.Test/InternetMovieDBTest.cs
+++ FilmLibrary.Test/InternetMovieDBTest.cs
@@ -1,6 +1,9 @@
 ﻿using FilmLibrary.InternetMovieDB;
 using FilmLibrary.InternetMovieDB.TheMovieDatabase;
 using FilmLibrary.InternetMovieDB.TheMovieDatabase.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 using Xunit;
 
 namespace FilmLibrary.Test
@@ -10,15 +13,32 @@ namespace FilmLibrary.Test
         [Fact]
         public async void Test()
         {
-            string apiKey = "";
             string baseUrl = "https://api.themoviedb.org";
             string searchMovieUrl = "3/search/movie";
             string getMovieUrl = "3/movie";
 
-            IHttpClient httpClient = new TheMovieDBHttpClient();
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+                .AddEnvironmentVariables()
+                .Build()
+                .Get<AppSettings>();
+
+            IHttpClient httpClient;
+            if (!string.IsNullOrEmpty(config.Proxy_url) && 
+                !string.IsNullOrEmpty(config.Proxy_username) && 
+                !string.IsNullOrEmpty(config.Proxy_password))
+            {
+                httpClient = new TheMovieDBHttpClient(config.Proxy_url, config.Proxy_username, config.Proxy_password);
+            }
+            else
+            {
+                httpClient = new TheMovieDBHttpClient();
+            }
+
             IMovieService<MovieSearchResult, MovieResult> movieService = new TheMovieDB<MovieSearchResult, MovieResult>(
                 httpClient,
-                apiKey,
+                config.Themoviedb_apikey,
                 baseUrl,
                 searchMovieUrl,
                 getMovieUrl);
